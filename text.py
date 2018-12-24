5221afe64d5459893e08063a3c337f35ec20cdef
blackcake <cassie100101@163.com>
2018-12-24 11:57:01 +0800

    测试git

---
+++ text.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-12-24 11:53:36
+# @Author  : Tang Yuzhe (${email})
+# @Link    : ${link}
+# @Version : $Id$
+
+import os
+import pygame
+from pygame.locals import *
